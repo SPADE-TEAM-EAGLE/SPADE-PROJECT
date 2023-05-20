f9163cd3c657ccbd6621dd26ddaf4ad65beccbc0
Romail Butt <romailbutt8@gmail.com>
2023-05-20 19:47:49 +0500

    dark and light mode , responsive

--- spade API 2/config/connection.js
+++ spade API 2/config/connection.js
@@ -2,27 +2,27 @@ const mysql = require('mysql2/promise');
 
 let connection;
 
-const createConnection= async()=>{
-  if (connection) return connection;
+const createConnection = async() => {
+    if (connection) return connection;
 
-  connection = await mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'spaid'
-  });
+    connection = await mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'spade'
+    });
 
-  return connection;
+    return connection;
 }
-const connect= async()=> {
+const connect = async() => {
     try {
-      const connection = await createConnection();
-      if(connection){
-        console.log("Connected to Database")
-      }
-      // Use the connection object here
+        const connection = await createConnection();
+        if (connection) {
+            console.log("Connected to Database")
+        }
+        // Use the connection object here
     } catch (err) {
-      console.error('Error connecting to database:', err);
+        console.error('Error connecting to database:', err);
     }
-  }
-module.exports = { createConnection,connect };
+}
+module.exports = { createConnection, connect };
