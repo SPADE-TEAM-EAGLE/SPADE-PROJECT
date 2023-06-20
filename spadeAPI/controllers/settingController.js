846aa2bfeeaa9719e6031215657f0d01d6c05d0c
UmairNazakat <umairnazakat2222@gmail.com>
2023-06-20 22:28:54 +0500

    Setting change password API is ready

---
+++ spadeAPI/controllers/settingController.js
@@ -0,0 +1,51 @@
+const user = require("../models/user");
+const sendMail = require('../sendmail/sendmail.js');
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt');
+const fs = require('fs');
+const Path = require('path');
+const imageToDelete = require('./../middleware/deleteImage.js')
+const { serialize } = require('cookie');
+const {
+    selectQuery,
+    deleteQuery,
+    updatePassword
+} = require("../constants/queries");
+const { hashedPassword } = require("../helper/hash");
+const { queryRunner } = require("../helper/queryRunner");
+const config = process.env;
+
+  //  ############################# Update Setting Password Start ############################################################ 
+exports.changePasssword = async function (req, res) {
+    // const {currentPassword, NewPassword } = req.query;
+    const {currentPassword, NewPassword } = req.body;
+    // const {userId}=req.user
+    const {userId}=req.body
+    const currentDate = new Date();
+
+    try {
+      
+        const selectResult = await queryRunner(selectQuery("users", "id"), [userId]);
+        if (selectResult[0].length === 0) {
+          res.status(400).send("User Not Found");
+        } else if (
+          await bcrypt.compare(currentPassword, selectResult[0][0].Password)
+        ) {
+            const hashPassword = await hashedPassword(NewPassword);
+            const updateResult = await queryRunner(updatePassword, [hashPassword,currentDate,userId]);
+              if (updateResult[0].affectedRows === 0) {
+                res.status(400).send("Error");
+              } else {
+                res.status(200).json({
+                    message: "Successful password Change",
+                  });
+              }
+        } else {
+          res.status(400).send("Incorrect Password");
+        }  
+    } catch (error) {
+      console.log(error);
+      res.status(400).send(error.message);
+    }
+  };
+  //  ############################# Update Setting Password END ############################################################
