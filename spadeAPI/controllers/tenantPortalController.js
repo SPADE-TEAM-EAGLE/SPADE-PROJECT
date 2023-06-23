6edd970c4a528127a45cf400583ca03e918c9e5b
Muhammad Fahad <106611736+MFahad11@users.noreply.github.com>
2023-06-23 19:07:06 +0500

    Integration fixes and JQUERY functionality fixes

--- spadeAPI/controllers/tenantPortalController.js
+++ spadeAPI/controllers/tenantPortalController.js
@@ -94,6 +94,7 @@ exports.getAllTaskTenant = async (req, res) => {
                 ID : vendorResult[0][0].id,
                 name: vendorResult[0][0].firstName + " "+ vendorResult[0][0].lastName,
                 email: vendorResult[0][0].email,
+                vendorPhone:vendorResult[0][0].phone
               };
               vendorData.push(vendor);
             }
