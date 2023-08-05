e51b361a184d136d087d3a8b1e90d8b56edda9d2
UmairNazakat <umairnazakat2222@gmail.com>
2023-08-05 17:14:36 +0500

    Payment API is ready

--- spadeAPI/app.js
+++ spadeAPI/app.js
@@ -5,15 +5,11 @@ const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const userRoutes = require("./routes/userRoutes");
 const { connect } = require("./config/connection");
-
-const recurringController = require("./controllers/recurringController"); 
+const recurringController = require("./controllers/recurringController");
+const { config } = require("dotenv");
+ 
 const app = express();
-// const server = http.createServer(app);
-// const io = new Server(server);
-
-// const options = {
-//     origin: ["https://backend.app.spaderent.com"],
-// }
+ 
 app.use(cookieParser());
 app.use(bodyParser.json());
 
@@ -28,29 +24,7 @@ app.use((req, res, next) => {
 app.use("/api/spade", userRoutes);
 connect();
 recurringController.start();
-
-// io.on("connection", (socket) => {
-//     console.log("Connected to socket.io");
-//     socket.on('join chat', (room) => {
-//         socket.join(room);
-//         console.log("User Joined Room: " + room);
-//     });
-
-//     socket.on("leave chat", (room) => {
-//         socket.leave(room);
-//         console.log("User left Room: " + room);
-//     });
-    
-//     socket.on("new message", (data) => {
-//         console.log(data);
-//         const senderId = data.chat._id;
-//         console.log('room id', senderId);
-//         chats = data.chat;
-
-//         io.in(data.chat._id).emit("message-recieved", data);
-//     });
-// })
-
+ 
 app.listen(3000, () => {
     console.log('listening on *:3000');
 });
---
+++ spadeAPI/helper/paymentIntegration.js
@@ -0,0 +1,74 @@
+const config = process.env;
+const safecharge = require('safecharge');
+const { request } = require('https'); // Use the built-in https module for HTTPS requests
+const sha256 = require('js-sha256');
+safecharge.initiate(config.merchantId, config.merchantSiteId, config.Secret_Key);
+
+// ############################ timestamp #################################
+function getTimestamp() {
+    const now = new Date();
+    const year = now.getFullYear();
+    const month = String(now.getMonth() + 1).padStart(2, '0');
+    const day = String(now.getDate()).padStart(2, '0');
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const seconds = String(now.getSeconds()).padStart(2, '0');
+    return `${year}${month}${day}${hours}${minutes}${seconds}`;
+  }
+  const timestamp = getTimestamp(); 
+// ############################ timestamp #################################
+
+exports.openOrder = async (req,res) => {
+    const {} = req.body;
+  const apiUrl = "https://ppp-test.nuvei.com/ppp/api/v1/openOrder.do";
+  const requestData = {
+    merchantId: "6400701569295268447",
+    merchantSiteId: "244298",
+    clientRequestId: "561ccf70-336b-11ee-a309-4f00ef0ed1ad",
+    clientUniqueId: "251003981",
+    currency: "USD",
+    amount: "200",
+    // userTokenId: "230811147",
+    timeStamp: timestamp,
+    checksum: sha256('6400701569295268447244298561ccf70-336b-11ee-a309-4f00ef0ed1ad200USD'+timestamp+'xp8GrYWC6n9wHbxWuDwRPtAPICRLbBvvY2DuLYVRu8v5ip4GHPNymd0MA8KsEpbU')
+  };
+
+  const requestOptions = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    }
+  };
+
+  const reqq = request(apiUrl, requestOptions, (response) => {
+    let responseData = ''; 
+    response.on('data', (chunk) => {
+      responseData += chunk;
+    }); 
+    response.on('end', () => {
+      try {
+        const data = JSON.parse(responseData);
+        // console.log(data);
+        res.status(200).json({
+            sessionToken:data.sessionToken
+        })
+      } catch (error) {
+        console.error('Error parsing response:', error);
+        res.status(400).json({
+            message:"Error parsing response",
+            error,
+        })
+      }
+    });
+  }); 
+  reqq.on('error', (error) => {
+    console.error('Error sending request:', error);
+    res.status(400).json({
+        message:"Error sending request",
+        error,
+    })
+  }); 
+  // Send the request body
+  reqq.write(JSON.stringify(requestData));
+  reqq.end();
+};
