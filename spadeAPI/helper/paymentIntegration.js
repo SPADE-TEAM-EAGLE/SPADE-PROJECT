48d3cb37d17b19e6cf5e610a04d564cdfc9d0eaf
Muhammad Fahad <106611736+MFahad11@users.noreply.github.com>
2023-09-26 11:38:25 +0500

    New design changes

--- spadeAPI/helper/paymentIntegration.js
+++ spadeAPI/helper/paymentIntegration.js
@@ -88,7 +88,7 @@ exports.openOrder = async (req,res) => {
     clientUniqueId: config.clientUniqueId,
     currency: currency,
     amount: amount,
-    transactionType: "Auth",
+    // transactionType: "Auth",
     userTokenId:"12345",
     timeStamp: timestamp,
     checksum: sha256(config.merchantId+config.merchantSiteId+config.clientRequestId+amount+currency+timestamp+config.Secret_Key)
