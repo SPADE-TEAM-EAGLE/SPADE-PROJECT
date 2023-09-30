64910417594c4ac4d6e31d9f75d852fb68ebac01
RomailButt <romailbutt8@gmail.com>
2023-09-30 14:05:28 +0500

    30 start

--- spadeAPI/middleware/imageUploads.js
+++ spadeAPI/middleware/imageUploads.js
@@ -7,35 +7,20 @@ const Path = require('path');
             cb(null,"uploads")
         },
         filename : function(req,file,cb){
-          // console.log(file)
+        
             cb(null, file.fieldname+`-`+ Date.now()+ Path.extname(file.originalname));
         }
     })
 }).array("image");
-// const upload = multer({
-//   storage: multer.diskStorage({
-//     destination: function (req, file, cb) {
-//       const uploadFolderPath = Path.join(__dirname, '..', 'uploads'); // Relative path to the destination folder
-//       cb(null, uploadFolderPath);
-//     },
-//     filename: function (req, file, cb) {
-//       // Generate the filename for the uploaded file
-//       cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
-//     }
-//   })
-// }).array('image');
 const uploadExistingFiles = async (req, res, next) => {
     try {
-    //   console.log(req)
-    //   let { existingImages } = req.body;
-    //   console.log(existingImages)
         existingImages=existingImages.split(",")
       if (existingImages && existingImages.length > 0) {
         const existingFiles = existingImages.map((imageName) => ({
           fieldname: 'image',
           originalname: imageName,
           encoding: '7bit',
-          mimetype: 'image/png', // Update the mimetype as per your image type
+          mimetype: 'image/png',
           destination: 'uploads',
           filename: imageName,
           path: `uploads/${imageName}`
