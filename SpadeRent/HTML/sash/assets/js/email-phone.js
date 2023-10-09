93e5e6adc1740b3b9881461d4270385c08d7357f
RomailButt <romailbutt8@gmail.com>
2023-10-09 16:03:42 +0500

    9 start

--- SpadeRent/HTML/sash/assets/js/email-phone.js
+++ SpadeRent/HTML/sash/assets/js/email-phone.js
@@ -1,10 +1,9 @@
 // Email validation
+var emailRegex =/^[A-Za-z0-9._%+-]+@[A-Za-z.-]+\.[A-Za-z]{2,}$/;
 $(document).on('input', 'input[type="email"]', function () {
   var email = $(this).val();
-  var emailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z.-]+\.[A-Za-z]{2,}$/;
   var email_span = $(this).siblings('.text-danger');
   if (!emailRegex.test(email)) {
-      // console.log("invalid");
       $(this).addClass("border-danger").removeClass("border-green");
       email_span.removeClass('d-none');
   } else {
@@ -52,28 +51,49 @@ $('input[type="tel"]').each(function () {
   });
 });
 
-$(document).on('input change', '.modal input[required]:not([type="email"]):not([type="tel"])', function () {
-  var inputText = $(this).val();
-  var inputText_span = $(this).siblings('.text-danger');
-  if (inputText == '') {
-      $(this).addClass("border-danger").removeClass("border-green");
-      inputText_span.removeClass('d-none');
-  } else {
-      $(this).addClass("border-green").removeClass("border-danger");
-      inputText_span.addClass('d-none');
-  }
-});
+// $(document).on('input change', '.modal input[required]:not([type="email"]):not([type="tel"])', function () {
+//   var inputText = $(this).val();
+//   var inputText_span = $(this).siblings('.text-danger');
+//   if (inputText == '') {
+//       $(this).addClass("border-danger").removeClass("border-green");
+//       inputText_span.removeClass('d-none');
+//   } else {
+//       $(this).addClass("border-green").removeClass("border-danger");
+//       inputText_span.addClass('d-none');
+//   }
+// });
 
 // select field validation
-$(document).on('change', '.modal select[required]', function () {
-  var selectText = $(this).val();
-  var selectText_span = $(this).siblings('.text-danger');
-  console.log(selectText);
-  if (selectText == 'Choose...') {
-      $(this).addClass("border-danger").removeClass("border-green");
-      selectText_span.removeClass('d-none');
-  } else {
-      $(this).addClass("border-green").removeClass("border-danger");
-      selectText_span.addClass('d-none');
-  }
+// $(document).on('change', '.modal select[required]', function () {
+//   var selectText = $(this).val();
+//   var selectText_span = $(this).siblings('.text-danger');
+//   console.log(selectText);
+//   if (selectText == 'Choose...') {
+//       $(this).addClass("border-danger").removeClass("border-green");
+//       selectText_span.removeClass('d-none');
+//   } else {
+//       $(this).addClass("border-green").removeClass("border-danger");
+//       selectText_span.addClass('d-none');
+//   }
+// });
+
+$(document).on('input change', '.modal input[required]:not([type="tel"]) , .modal select[required]', function () {
+
+  var modalValid = $(this).val();
+  var modalValid_span = $(this).siblings('.text-danger');
+ if(
+  ($(this).is('input') && modalValid == '') || 
+  ($(this).is('select') && modalValid == 'Choose...') || 
+  ($(this).is('input[type="email"]') && !emailRegex.test(modalValid)) ||
+  ($(this).is('input[name="zip"]') && modalValid.length != 5) 
+){
+  console.log("checker not Ok");
+  $(this).removeClass("border-green").addClass("border-danger");
+  modalValid_span.removeClass('d-none');
+ }else{
+  console.log("checker Ok");
+
+  $(this).removeClass("border-danger").addClass("border-green");
+  modalValid_span.addClass('d-none');
+ }
 });
