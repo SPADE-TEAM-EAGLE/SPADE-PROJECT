0b54a2ac665a3dfb6248fb0c38ee2bf9dddddbd2
Muhammad Fahad <106611736+MFahad11@users.noreply.github.com>
2023-10-12 16:17:31 +0500

    Payment subsription

--- spadeAPI/helper/paymentIntegration.js
+++ spadeAPI/helper/paymentIntegration.js
@@ -268,3 +268,53 @@ exports.getUserDetailsPayment = async (req,res) => {
   reqq.end();
   };
   // ###################################### Get USER Details #############################################################
+
+
+  exports.createSubscription = async (req,res) => {
+    const requestData = {
+      merchantId: config.merchantId,
+      merchantSiteId: config.merchantSiteId,
+      userTokenId: "1235",
+      clientRequestId: config.clientRequestId,
+      timeStamp: timestamp,
+      checksum: sha256(config.merchantId+config.merchantSiteId+"1235"+config.clientRequestId+timestamp+config.Secret_Key),
+    };
+    console.log(requestData.checksum);
+    const requestOptions = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    };
+    const reqq = request("https://ppp-test.nuvei.com/ppp/api/v1/getUserDetails.do", requestOptions, (response) => {
+      let responseData = ''; 
+      response.on('data', (chunk) => {
+        responseData += chunk;
+      }); 
+      response.on('end', () => {
+        try {
+          console.log(responseData);
+          const data = JSON.parse(responseData);
+          res.status(200).json({
+              data
+          })
+        } catch (error) {
+          console.error('Error parsing response:', error);
+          res.status(400).json({
+              message:"Error parsing response",
+              error,
+          })
+        }
+      });
+    }); 
+    reqq.on('error', (error) => {
+      console.error('Error sending request:', error);
+      res.status(400).json({
+          message:"Error sending request",
+          error,
+      })
+    }); 
+    reqq.write(JSON.stringify(requestData));
+    reqq.end();
+    };
+
