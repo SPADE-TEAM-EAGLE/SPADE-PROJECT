d6b373aa1c0f6458ccc089b234ca28535752374c
UmairNazakat <umairnazakat2222@gmail.com>
2023-10-26 17:25:41 +0500

    Update Annually subscription pro rate

--- spadeAPI/helper/paymentIntegration.js
+++ spadeAPI/helper/paymentIntegration.js
@@ -658,13 +658,15 @@ function formatDateForSQL(date) {
     }
   }
   let daysDifferenceAnnually;
-  if (planId > PlanID && monthlyAnnual == "Annually") {
+  if (planId < PlanID && monthlyAnnual == "Annually") {
+    console.log("planId " + planId + "PlanID " + PlanID)
     const { subscriptionCreated_at, PlanID } = UserResult[0][0];
 const currentDate = new Date();
 const timeDifference = currentDate.getTime() - subscriptionCreated_at.getTime();
  daysDifferenceAnnually = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
 const monthsDifference = (currentDate.getMonth() + 1) - (subscriptionCreated_at.getMonth() + 1) + (currentDate.getFullYear() - subscriptionCreated_at.getFullYear()) * 12;
 //
+console.log("daysDifferenceAnnually " + daysDifferenceAnnually);
       let remainingDays = daysDifferenceAnnually;
       remainingDays = 365 - remainingDays;
       let initialAmountChange = existPlanAmount / 365;
@@ -722,6 +724,7 @@ const monthsDifference = (currentDate.getMonth() + 1) - (subscriptionCreated_at.
   if (planId < UserResult[0][0].PlanID && monthlyAnnual == "Annually") {
 
     let AddDays = 365 - daysDifferenceAnnually;
+
     subscriptionDate.setDate(subscriptionDate.getDate() + AddDays);
   
     requestData.startAfter = {
