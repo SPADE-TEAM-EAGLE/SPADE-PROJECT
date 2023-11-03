e3f00f44d1cb05c950f264c72fbd1ec3e876aa7b
UmairNazakat <umairnazakat2222@gmail.com>
2023-11-03 18:17:00 +0500

    Add email code

--- spadeAPI/sendmail/paymentMail.js
+++ spadeAPI/sendmail/paymentMail.js
@@ -4,7 +4,11 @@ exports.paymentHTML = (Name,subscriptionDate,Amount,planName) => {
 <html xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office">
 
 <head>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Figtree&display=swap" rel="stylesheet">
 
+  
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="x-apple-disable-message-reformatting">
@@ -14,9 +18,9 @@ exports.paymentHTML = (Name,subscriptionDate,Amount,planName) => {
     <title>Email</title>
 
 </head>
-  <body style="text-align: center;margin: 0;padding-top: 10px;padding-bottom: 10px;padding-left: 0;padding-right: 0;-webkit-text-size-adjust: 100%;background-color: #f2f4f6;color: #000000;background-image: url(https://spades3bucket.s3.amazonaws.com/1697540494792_Frame-2.jpg);background-repeat: no-repeat;">
-  <div style="text-align: center;">
-    <table align="center" style="box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px; text-align: center; vertical-align: top; width: 100%; max-width: 600px; background-color: #ffffff; border-radius: 20px 20px 0 0 ;">
+  <body style="font-family: 'Figtree'; text-align: left; margin: 0;padding-top: 10px;padding-bottom: 10px;padding-left: 0;padding-right: 0;-webkit-text-size-adjust: 100%;background-color: #f2f4f6;color: #000000;background-image: url(https://spades3bucket.s3.amazonaws.com/1697540494792_Frame-2.jpg);background-repeat: no-repeat;">
+  <div style="text-align: left;">
+    <table align="center" style="box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px; text-align: left; vertical-align: top; width: 100%; max-width: 600px; background-color: #ffffff; border-radius: 20px 20px 0 0 ;">
       <tbody>
         <tr>
           <td style="width: 596px; vertical-align: top; padding-left: 0; padding-right: 0; padding-top: 15px; padding-bottom: 15px;" width="596">
@@ -51,22 +55,14 @@ exports.paymentHTML = (Name,subscriptionDate,Amount,planName) => {
     <!-- <table align="center" style="box-shadow:rgba(0, 0, 0, 0.1) 0px 4px 12px; text-align: center; vertical-align: top; width: 900px; max-width: 900px; background-color: #ffffff; border-radius: 0 0 20px 20px;" width="600">
         <tbody>
           <tr> -->
-            <table align="center" style="box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px; text-align: center; vertical-align: top; width: 100%; max-width: 600px; background-color: #ffffff; border-radius: 0 0 20px 20px;">
+            <table align="center" style="box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px; text-align: left; vertical-align: top; width: 100%; max-width: 600px; background-color: #ffffff; border-radius: 0 0 20px 20px;">
               <tbody>
                   <tr>
-            <!-- <td style="width: 596px; vertical-align: top; padding-left: 30px; padding-right: 30px; padding-top: 30px; padding-bottom: 40px;" width="596">
-
-              <h1 style="font-size: 20px; line-height: 24px; font-family: 'Helvetica', Arial, sans-serif; font-weight: 600; text-decoration: none; color: #000000; text-align: start">Dear ${Name},</h1>
-
-              <p style="font-size: 15px; line-height: 24px; font-family: 'Helvetica', Arial, sans-serif; font-weight: 400; text-decoration: none; color: black; text-align: start">We're thrilled to welcome you to the <b>Spade Rent</b> family! Thank you for subscribing to our amazing service.</p>              
-              <p style="font-size: 15px; line-height: 24px; font-family: 'Helvetica', Arial, sans-serif; font-weight: 400; text-decoration: none; color: black; text-align: start">We're excited to have you on board as a subscriber to our property management services. Your journey with us is just beginning, and we are here to make your experience as a property owner or tenant as smooth and enjoyable as possible.</p>              
-           
-            </td>
-          </tr> -->
+            
           <td style="width: 100%; vertical-align: top; padding: 30px;">
-            <h1 style="font-size: 20px; line-height: 24px; font-family: 'Helvetica', Arial, sans-serif; font-weight: 600; text-decoration: none; color: #000000; text-align: start;">Dear ${Name},</h1>
-            <p style="font-size: 15px; line-height: 24px; font-family: 'Helvetica', Arial, sans-serif; font-weight: 400; text-decoration: none; color: black; text-align: start;">We're thrilled to welcome you to the <b>Spade Rent</b> family! Thank you for subscribing to our amazing service.</p>
-            <p style="font-size: 15px; line-height: 24px; font-family: 'Helvetica', Arial, sans-serif; font-weight: 400; text-decoration: none; color: black; text-align: start;">We're excited to have you on board as a subscriber to our property management services. Your journey with us is just beginning, and we are here to make your experience as a property owner or tenant as smooth and enjoyable as possible.</p>
+            <h1 style="font-size: 20px; line-height: 24px; font-family: 'Figtree'; font-weight: 600; text-decoration: none; color: #000000; text-align: start;">Dear ${Name},</h1>
+            <p style="font-size: 15px; line-height: 24px; font-family: 'Figtree'; font-weight: 400; text-decoration: none; color: black; text-align: start;">We're thrilled to welcome you to the <b>Spade Rent</b> family! Thank you for subscribing to our amazing service.</p>
+            <p style="font-size: 15px; line-height: 24px; font-family: 'Figtree'; font-weight: 400; text-decoration: none; color: black; text-align: start;">We're excited to have you on board as a subscriber to our property management services. Your journey with us is just beginning, and we are here to make your experience as a property owner or tenant as smooth and enjoyable as possible.</p>
         </td>
     </tr>
           <tr>
@@ -265,6 +261,5 @@ exports.paymentHTML = (Name,subscriptionDate,Amount,planName) => {
 
   </body>
 
-</html>
-        `
+</html>`
     };
