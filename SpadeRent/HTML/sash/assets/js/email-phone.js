7f06e2c0bb16275dd0d76d8606195e375dd6379a
RomailButt <romailbutt8@gmail.com>
2023-11-23 15:49:15 +0500

    user perission half

--- SpadeRent/HTML/sash/assets/js/email-phone.js
+++ SpadeRent/HTML/sash/assets/js/email-phone.js
@@ -1,40 +1,54 @@
 // Email validation
-var emailRegex =/^[A-Za-z0-9._%+-]+@[A-Za-z.-]+\.[A-Za-z]{2,}$/;
-$(document).on('input', 'input[type="email"]', function () {
+var emailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z.-]+\.[A-Za-z]{2,}$/;
+$(document).on("input", 'input[type="email"]', function () {
   var email = $(this).val();
-  var email_span = $(this).siblings('.text-danger');
+  var email_span = $(this).siblings(".text-danger");
   if (!emailRegex.test(email)) {
-    console.log('email not ok');
-      $(this).addClass("border-danger").removeClass("border-green");
-      email_span.removeClass('d-none');
-  } else {
-    console.log('email  ok');
-
-      $(this).addClass("border-green").removeClass("border-danger");
-      email_span.addClass('d-none');
-  }
-});
-$(document).on('input change', 'input:not([type="tel"]):not(.modal *):not(#typehead,#typehead1,#typehead2,#search_input,#typehead_users), select:not(#lengthSelectBox, #lengthSelectBox1, #lengthSelectBox2, #lengthSelectBox3, #lengthSelectBox4, #lengthSelectBox5):not(.modal *)', function () {
-  var input_select_value = $(this).val();
-  var input_select_value_span = $(this).siblings('.text-danger');
-  if (input_select_value === '') {
-    console.log('input or select is empty');
+    console.log("email not ok");
     $(this).addClass("border-danger").removeClass("border-green");
+    email_span.removeClass("d-none");
   } else {
-    console.log('input or select is filled');
+    console.log("email  ok");
+
     $(this).addClass("border-green").removeClass("border-danger");
+    email_span.addClass("d-none");
   }
 });
+$(document).on(
+  "input change",
+  'input:not([type="tel"]):not(.modal *):not(#typehead,#typehead1,#typehead2,#search_input,#typehead_users), select:not(#lengthSelectBox, #lengthSelectBox1, #lengthSelectBox2, #lengthSelectBox3, #lengthSelectBox4, #lengthSelectBox5):not(.modal *)',
+  function () {
+    var input_select_value = $(this).val();
+    var input_select_value_span = $(this).siblings(".text-danger");
+    if (input_select_value === "") {
+      console.log("input or select is empty");
+      $(this).addClass("border-danger").removeClass("border-green");
+    } else {
+      console.log("input or select is filled");
+      $(this).addClass("border-green").removeClass("border-danger");
+    }
+  }
+);
 
-const errorMap = ["Invalid number", "Invalid country code", "Too short", "Too long", "Invalid number"];
+const errorMap = [
+  "Invalid number",
+  "Invalid country code",
+  "Too short",
+  "Too long",
+  "Invalid number",
+];
 
 $('input[type="tel"]').each(function () {
   const input = $(this);
-  const errorMsg = input.next(".error-msg").add(input.closest("div").siblings(".error-msg"));
-  const validMsg = input.next(".valid-msg").add(input.closest("div").siblings(".valid-msg"));
+  const errorMsg = input
+    .next(".error-msg")
+    .add(input.closest("div").siblings(".error-msg"));
+  const validMsg = input
+    .next(".valid-msg")
+    .add(input.closest("div").siblings(".valid-msg"));
 
   const iti = window.intlTelInput(this, {
-    utilsScript: "/intl-tel-input/js/utils.js?1684676252775"
+    utilsScript: "/intl-tel-input/js/utils.js?1684676252775",
   });
 
   const reset = () => {
@@ -44,16 +58,23 @@ $('input[type="tel"]').each(function () {
     validMsg.addClass("d-none");
   };
 
-  input.on('input', () => {
+  input.on("input", () => {
     reset();
     if (input.val().trim()) {
       if (iti.isValidNumber()) {
-        console.log("valid phone");
+        const formattedNumber = intlTelInputUtils.formatNumber(
+          iti.getNumber(),
+          iti.getSelectedCountryData().iso2,
+          intlTelInputUtils.numberFormat.NATIONAL
+        );
+        input.val(formattedNumber);
+        console.log("Valid phone. Formatted number:", formattedNumber);
+
         validMsg.removeClass("d-none");
         input.removeClass("border-danger");
         input.addClass("border-green");
       } else {
-        console.log("invalid phone");
+        console.log("Invalid phone");
         input.addClass("border-danger");
         input.removeClass("border-green");
         input.addClass("error");
@@ -90,42 +111,43 @@ $('input[type="tel"]').each(function () {
 //       sel  ectText_span.addClass('d-none');
 //   }
 // });
-$(document).ready(function(){
-
+$(document).ready(function () {
   var modalOpen = false;
-  
-  $('.modal').on('shown.bs.modal', function () {
+
+  $(".modal").on("shown.bs.modal", function () {
     modalOpen = true;
   });
-  
-  $('.modal').on('hidden.bs.modal', function () {
+
+  $(".modal").on("hidden.bs.modal", function () {
     modalOpen = false;
   });
-  
-  $('.modal input[required]:not([type="tel"]), .modal select[required]').on('input change', function () {
-    if (modalOpen) {
-      var modalValid = $(this).val();
-      var modalValid_span = $(this).siblings('.text-danger');
-      if (
-        ($(this).is('input') && modalValid === '') || 
-        ($(this).is('select') && modalValid === 'Choose...') || 
-        ($(this).is('input[type="email"]') && !emailRegex.test(modalValid)) ||
-        ($(this).is('input[name="zip"]') && modalValid.length !== 5) 
-      ) {
-        console.log("checker not Ok");
-        $(this).removeClass("border-green").addClass("border-danger");
-        modalValid_span.text('This field is required!');
-        modalValid_span.removeClass('d-none');
-      } else {
-        console.log("checker Ok");
-        $(this).removeClass("border-danger").addClass("border-green");
-        modalValid_span.text('This field is required!');
-        modalValid_span.addClass('d-none');
+
+  $('.modal input[required]:not([type="tel"]), .modal select[required]').on(
+    "input change",
+    function () {
+      if (modalOpen) {
+        var modalValid = $(this).val();
+        var modalValid_span = $(this).siblings(".text-danger");
+        if (
+          ($(this).is("input") && modalValid === "") ||
+          ($(this).is("select") && modalValid === "Choose...") ||
+          ($(this).is('input[type="email"]') && !emailRegex.test(modalValid)) ||
+          ($(this).is('input[name="zip"]') && modalValid.length !== 5)
+        ) {
+          console.log("checker not Ok");
+          $(this).removeClass("border-green").addClass("border-danger");
+          modalValid_span.text("This field is required!");
+          modalValid_span.removeClass("d-none");
+        } else {
+          console.log("checker Ok");
+          $(this).removeClass("border-danger").addClass("border-green");
+          modalValid_span.text("This field is required!");
+          modalValid_span.addClass("d-none");
+        }
       }
     }
-  });
-})
-
+  );
+});
 
 // $('input[type="date"]').on('change',function(){
 //   var dateValue = $(this).val().split('-');
@@ -134,28 +156,51 @@ $(document).ready(function(){
 // $(this).val(assignDateValue)
 // })
 
-$('#addModal, #myModal_edit,#add-user-permissions,#edit-user-permissions').on('hidden.bs.modal', function() {
-  $("#error-msg").addClass('d-none')
-  $("#valid-msg").addClass('d-none')
-  for (var indexx = 0; indexx < 10; indexx++) {
-    $("#error-msg"+ indexx).addClass('d-none')
-    $("#valid-msg" +indexx).addClass('d-none')
+$("#addModal, #myModal_edit,#add-user-permissions,#edit-user-permissions").on(
+  "hidden.bs.modal",
+  function () {
+    $("#error-msg").addClass("d-none");
+    $("#valid-msg").addClass("d-none");
+    for (var indexx = 0; indexx < 10; indexx++) {
+      $("#error-msg" + indexx).addClass("d-none");
+      $("#valid-msg" + indexx).addClass("d-none");
+    }
+    // var selectedFiles = [];
+    // console.log($(this).find(".file-grid"));
+    $(this)
+      .find('input:not([type="radio"]), select:not(#assignee_update),textarea')
+      .each(function () {
+        if ($(this).is("select")) {
+          $(this).val("Choose...");
+          $(this).removeClass("border-green").removeClass("border-danger");
+          $(this).siblings("span").addClass("d-none");
+          console.log($(this).siblings("span"));
+        } else {
+          $(this).val("");
+          $(this).removeClass("border-green").removeClass("border-danger");
+        }
+      });
   }
-  // var selectedFiles = [];
-  // console.log($(this).find(".file-grid"));
-  $(this).find('input:not([type="radio"]), select:not(#assignee_update),textarea').each(function(){
-   if($(this).is('select')) {
-    $(this).val('Choose...');
-    $(this).removeClass('border-green').removeClass('border-danger')
-    $(this).siblings('span').addClass('d-none')
-    console.log($(this).siblings('span'));
+);
+// $("input[type='date']").each(function(){
+//   $(this).attr("type","text")
+// })
+// function formatDateMDY1(date) {
+//   var createdDateFormat1 = new Date(date);
+//   var day1 = createdDateFormat1.getDate();
+//   var month1 = createdDateFormat1.getMonth() + 1;
+//   var year1 = createdDateFormat1.getFullYear();
+//   var createdDateFormatComplete1 = month1 + "/" + day1 + "/" + year1;
+//   return createdDateFormatComplete1;
+// }
 
-   }else{
-     $(this).val("")
-     $(this).removeClass('border-green').removeClass('border-danger')
-   }
-  })
-});
-
-
+// $("input[type='date']").on("change", function () {
+//   $(this).attr("type", "text");
+//   $(this).val(formatDateMDY1($(this).val()));
+//   $(this).on("input", function () {
+//     if ($(this).val() == "") { 
+//       $(this).attr("type", "date");
+//     }
+//   });
+// });
 
