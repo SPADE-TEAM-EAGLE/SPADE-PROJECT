6446ec4818c194e79c0037e49c3a6a301b98f14a
UmairNazakat <umairnazakat2222@gmail.com>
2023-12-27 12:09:27 +0500

    update APIs, change status code which was given be TAS team

--- spadeAPI/controllers/usersTaskController.js
+++ spadeAPI/controllers/usersTaskController.js
@@ -324,7 +324,7 @@ exports.getAllUserTask = async (req, res) => {
         message: "All Tasks",
       });
     } else {
-      res.status(200).json({
+      res.status(404).json({
         message: "No Tasks data found",
       });
     }
