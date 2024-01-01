1dab61db8d132304d37037fdc9c5c536cd1c2139
MFahad11 <fahadahad567@gmail.com>
2024-01-01 19:09:44 +0500

    bug fixing

--- spadeAPI/index.js
+++ spadeAPI/index.js
@@ -7,17 +7,27 @@ const userRoutes = require("./routes/userRoutes");
 const { connect } = require("./config/connection");
 const app = express();
 
-
 app.use(cookieParser());
 app.use(bodyParser.json());
-const corseOptions={
-  credentials:true,
-  optionSuccessStatus:200
-}
-app.use(cors(corseOptions));
 
+// Define multiple allowed origins
+const allowedOrigins = ["https://admin.socket.io", "https://app.spaderent.com", "https://backend.app.spaderent.com","https://admin.spaderent.com"]; // Add your allowed origins
 
-app.options("*",cors())
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true); // Allow the request
+    } else {
+      callback(new Error('Not allowed by CORS')); // Block the request
+    }
+  },
+  credentials: true,
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+
+app.options("*", cors(corsOptions));
 
 app.use((req, res, next) => {
   res.header("Cache-Control", "no-cache, no-store, must-revalidate");
@@ -25,13 +35,13 @@ app.use((req, res, next) => {
   res.header("Expires", "0");
   next();
 });
+
 app.use("/api/spade", userRoutes);
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
 connect();
 
-// Remove the app.listen() block
-
-module.exports = app; // Export the app object
+module.exports = app;
 
